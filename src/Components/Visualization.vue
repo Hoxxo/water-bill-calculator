b87904da7fb0baacd9e571355ba6c30260aa819b
Hiroshi Angelo <parasite242@gmail.com>
2023-05-15 00:11:39 +0900

    Added working visualizer

--- src/Components/Visualization.vue
+++ src/Components/Visualization.vue
@@ -1,45 +1,47 @@
 <template>
-  <div>
-    <line-chart :chart-data="chartData" :options="options"></line-chart>
-  </div>
+  <Bar
+      id="my-chart-id"
+      :options="chartOptions"
+      :data="chartData"
+  />
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue';
-import { Line } from 'vue-chartjs';
+import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-export default defineComponent({
-  name: 'MyChart',
-  components: {Line},
-  setup() {
-    const chartData = ref({
-      labels: [],
-      datasets: [
-        {
-          label: 'Data One',
-          backgroundColor: '#f87979',
-          data: [],
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    const times = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
+    const monData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+
+    const create_labels = (times) => times.map(time => time.split(':')[0]);
+    const create_data = (data) => data.map(item => ({x: item}));
+
+    return {
+      chartData: {
+        labels: times,
+        datasets: [
+            {
+              label: 'time',
+              data: monData,
+              borderWidth: 1
+            }
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        scales: {
+          x: {
+            beginAtZero: true,
+          },
         },
-      ],
-    });
-
-    const options = ref({
-      responsive: true,
-      maintainAspectRatio: false,
-    });
-
-    onMounted(async () => {
-      const response = await fetch('http://localhost:5000/data');
-      const data = await response.json();
-      chartData.value.labels = data.labels;
-      chartData.value.datasets[0].data = data.data;
-      console.log(chartData.value.datasets[0].data);
-    });
-
-    return {chartData, options};
+      },
+    }
   }
-});
+}
 </script>
