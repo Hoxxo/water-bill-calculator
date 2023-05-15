1936b6640f3a718580515b0582d55f7a6abb435d
Angelo <parasite242@gmail.com>
2023-05-15 10:36:16 +0900

    Created functions for making the data and the prototype for fetching the data from the google spreadsheets API

---
+++ src/Components/ts-functions/get-data.ts
@@ -0,0 +1,31 @@
+const fetch_data = () =>{
+
+}
+
+const generate_numbers = (n: number): number[] => {
+    let r: number[] = [];
+
+    for (let i = 0; i < n; i++) {
+        let num = Math.random() * 100
+        r.push(Math.floor(num));
+    }
+
+    return r;
+};
+
+let x = generate_numbers(10);
+let y = generate_numbers(10);
+
+type Coordinate = {x: number, y: number};
+
+const get_data = (x: number[], y: number[]): Coordinate[] => {
+    let r: Coordinate[] = [];
+
+    x.map((i, index) => {
+        r.push({x: i, y: y[index]})
+    });
+
+    return r;
+}
+
+console.log(get_data(x, y));
