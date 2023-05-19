d8ff706586b0cb80f5b3383315ba541af3c1839e
Angelo <parasite242@gmail.com>
2023-05-19 15:59:16 +0900

    Succesfully manipulated data at current day and current hour and summed up a column for a specified day

---
+++ py_code/data.py
@@ -0,0 +1,26 @@
+# import serial
+import openpyxl
+import pandas as pd
+from datetime import datetime
+import re
+
+path = R'/Users/hiroshi/PyCharmProjects/sandbox/py-data.xlsx'
+
+# ser = serial.Serial('path-to-serial', 9600)
+df = pd.read_excel(path, sheet_name='Sheet1')
+df = df.dropna(how='all')
+df = df.set_index(df.columns[0])
+df.index = df.index.astype(str)
+df.index.name = None
+df.columns = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+df = df.iloc[0:, :]
+now = datetime.now()
+hour = str(now.hour) + ':00:00'
+day = now.strftime('%A')[:3]
+
+numbers = []
+
+# Time column index
+df.at['Total', 'Tue'] = df['Tue'][:-1].sum()
+print(df.at['Total', 'Tue'])
+print(df.at['Total', 'Mon'])
