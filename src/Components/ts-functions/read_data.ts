dbd97eb2ca4ce14c36bd018385afbb213bd99c07
Angelo <parasite242@gmail.com>
2023-05-22 15:35:14 +0900

    Working on reading from excel file

--- src/Components/ts-functions/read_data.ts
+++ src/Components/ts-functions/read_data.ts
@@ -1,16 +1,19 @@
 import * as XLSX from 'xlsx';
 
-let book: XLSX.WorkBook | null = null;
 let sheet: XLSX.WorkSheet | null = null;
 
-(() => {
+let path: string = '/Users/hiroshi/PyCharmProjects/sandbox/py-data.xlsx'
+
+const open_sheet = (path: string): XLSX.WorkBook | null => {
     try {
-        book = XLSX.readFile('/Users/hiroshi/PyCharmProjects/sandbox/py-data.xlsx');
+        return XLSX.readFile(path);
     } catch (error) {
-        console.log("Error reading file!", error);
+        console.error("Error reading file!", error);
         return null;
     }
-})();
+}
+
+const book = open_sheet(path);
 
 if (book) {
     sheet = book.Sheets[book.SheetNames[0]];
