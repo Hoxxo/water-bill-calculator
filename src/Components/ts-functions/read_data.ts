cc48379237cbc99a5b84eb9fad0c0e8b8d4e0ba7
Hiroshi Angelo <parasite242@gmail.com>
2023-06-20 19:13:33 +0900

    Made the bar graph look better

--- src/Components/ts-functions/read_data.ts
+++ src/Components/ts-functions/read_data.ts
@@ -1,13 +1,16 @@
 import * as XLSX from 'xlsx'
 import * as dfd from 'danfojs-node'
 import * as fs from 'fs'
+import * as os from 'os'
 
 const current_day: string = new Date().toLocaleString('en-gb', { weekday: 'short' })
 
 // Mac: /Users/hiroshi/PyCharmProjects/sandbox/py-data.xlsx
 // Windows: C:\Users\hiroshi\PycharmProjects\sandbox\py-data.xlsx
 
-export const path: string = '/Users/hiroshi/PyCharmProjects/sandbox/py-data.xlsx'
+export const path: string = os.platform() === 'win32' ?
+  'C:\\Users\\chain\\PycharmProjects\\sandbox\\py-data.xlsx'
+  : '/Users/hiroshi/PyCharmProjects/sandbox/py-data.xlsx'
 
 export const load_data_frame = async (path: string): Promise<number[] | null> => {
   try {
@@ -20,7 +23,7 @@ export const load_data_frame = async (path: string): Promise<number[] | null> =>
     console.log(df[current_day].$dataIncolumnFormat.slice(0, -1))
     return df[current_day].$dataIncolumnFormat.slice(0, -1)
   } catch (error) {
-    console.error('Error reading file!', error)
+    console.error(`Error reading file! ${error}`)
     return null
   }
 }
